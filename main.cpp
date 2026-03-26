54983a6e114ebd6c46bab4c5ade94f65aaefc282
Andy Roxby <aroxby@users.noreply.github.com>
2026-03-26 01:06:53 -0400

    Format code

--- main.cpp
+++ main.cpp
@@ -3,19 +3,21 @@
 #include <iostream>
 #include <ranges>
 #include <sstream>
-#include <windows.h>
+
 #include <processthreadsapi.h>
+#include <windows.h>
+
 using namespace std;
 
-
+constexpr const char *EFI_GLOBAL_VARIABLE_GUID = "{8BE4DF61-93CA-11D2-AA0D-00E098032B8C}";
 constexpr int MAX_LOAD_OPTIONS = UINT16_MAX;
+
 typedef uint16_t LoadOptionId;
 struct LoadOption {
     uint32_t attributes;
     uint16_t filePathListLength;
     wchar_t description[];
 };
-
 
 // Based on:
 // https://github.com/microsoft/Windows-classic-samples/blob/98185/Samples/ManagementInfrastructure/cpp/Process/Provider/WindowsProcess.c#L49
@@ -25,26 +27,26 @@ BOOL enablePrivilege() {
     BOOL bRes = FALSE;
     HANDLE hToken = NULL;
     BYTE *pBuffer = NULL;
-    TOKEN_PRIVILEGES* pPrivs = NULL;
+    TOKEN_PRIVILEGES *pPrivs = NULL;
 
     bRes = LookupPrivilegeValue(NULL, SE_SYSTEM_ENVIRONMENT_NAME, &privilegeRequired);
-    if(!bRes) {
+    if (!bRes) {
         return FALSE;
     }
 
     bRes = OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY | TOKEN_ADJUST_PRIVILEGES, &hToken);
-    if(!bRes) {
+    if (!bRes) {
         return FALSE;
     }
 
     bRes = GetTokenInformation(hToken, TokenPrivileges, NULL, 0, &dwLen);
-    if(bRes) {
+    if (bRes) {
         CloseHandle(hToken);
         return FALSE;
     }
 
-    pBuffer = (BYTE*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, dwLen);
-    if(!pBuffer) {
+    pBuffer = (BYTE *)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, dwLen);
+    if (!pBuffer) {
         CloseHandle(hToken);
         return FALSE;
     }
@@ -58,12 +60,10 @@ BOOL enablePrivilege() {
 
     // Iterate through all the privileges and enable the one required
     bRes = FALSE;
-    pPrivs = (TOKEN_PRIVILEGES*)pBuffer;
-    for(iCount = 0; iCount < pPrivs->PrivilegeCount; iCount++) {
-        if (
-            pPrivs->Privileges[iCount].Luid.LowPart == privilegeRequired.LowPart &&
-            pPrivs->Privileges[iCount].Luid.HighPart == privilegeRequired.HighPart
-        ) {
+    pPrivs = (TOKEN_PRIVILEGES *)pBuffer;
+    for (iCount = 0; iCount < pPrivs->PrivilegeCount; iCount++) {
+        if (pPrivs->Privileges[iCount].Luid.LowPart == privilegeRequired.LowPart &&
+            pPrivs->Privileges[iCount].Luid.HighPart == privilegeRequired.HighPart) {
             pPrivs->Privileges[iCount].Attributes |= SE_PRIVILEGE_ENABLED;
             // here it's found
             bRes = AdjustTokenPrivileges(hToken, FALSE, pPrivs, dwLen, NULL, NULL);
@@ -83,11 +83,10 @@ BOOL enablePrivilege() {
 }
 
 DWORD getUEFIVar(const char *varName, auto &buffer) {
-    constexpr const char *EFI_GLOBAL_VARIABLE_GUID = "{8BE4DF61-93CA-11D2-AA0D-00E098032B8C}";
     DWORD length = GetFirmwareEnvironmentVariableA(varName, EFI_GLOBAL_VARIABLE_GUID, buffer, sizeof(buffer));
     if (length == 0) {
         DWORD err = GetLastError();
-        if(err == ERROR_ENVVAR_NOT_FOUND) {
+        if (err == ERROR_ENVVAR_NOT_FOUND) {
             cerr << "GetFirmwareEnvironmentVariable failed because the variable was not found." << endl;
         } else {
             cerr << "GetFirmwareEnvironmentVariable failed with error code: " << err << endl;
@@ -103,9 +102,9 @@ string optionNameFromId(LoadOptionId i) {
 }
 
 int mainNoPause() {
-    LoadOptionId BootOrder[MAX_LOAD_OPTIONS];
-    uint8_t LoadOptionBuffer[1024];
-    DWORD BootOrderLength;
+    LoadOptionId bootOrder[MAX_LOAD_OPTIONS];
+    uint8_t loadOptionBuffer[1024];
+    DWORD bootOrderLength;
     DWORD err;
 
     if (!enablePrivilege()) {
@@ -125,22 +124,22 @@ int mainNoPause() {
     BootNext NV, BS, RT The boot option for the next boot only.
     BootOrder NV, BS, RT The ordered boot option load list
     */
-    BootOrderLength = getUEFIVar("BootOrder", BootOrder);
-    if (BootOrderLength == 0) {
+    bootOrderLength = getUEFIVar("BootOrder", bootOrder);
+    if (bootOrderLength == 0) {
         return 2;
     }
 
     cout << "BootOrder:\n";
-    for(auto e: BootOrder | std::views::take(BootOrderLength / sizeof(BootOrder[0]))) {
+    for (auto e : bootOrder | std::views::take(bootOrderLength / sizeof(bootOrder[0]))) {
         const char *optionName = optionNameFromId(e).c_str();
-        cout << optionName << endl;
-        auto len = getUEFIVar(optionName, LoadOptionBuffer);
+        cout << "> " << optionName << ":" << endl;
+        auto len = getUEFIVar(optionName, loadOptionBuffer);
         if (len == 0) {
             return 3;
         }
-        LoadOption *option = (LoadOption*)LoadOptionBuffer;
-        wcout << option->description << endl;
-        cout << "Attributes: " << option->attributes << endl;
+        LoadOption *option = (LoadOption *)loadOptionBuffer;
+        wcout << L">> Description: " << option->description << endl;
+        cout << ">> Attributes: " << option->attributes << endl;
     }
 
     return 0;
